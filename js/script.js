27cad0503dbbc9a13e8738d13dd0891b017ba6dc
maria-luiza-duda <maria.duda@terceiro-sky.com.br>
2021-08-17 23:07:51 -0300

    atualização css

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector("#items").addEventListener("wheel", event => {
+    if(event.deltaY > 0) {
+        console.log("scroll up")
+    } else {
+        console.log("scroll down")
+    }
+})
